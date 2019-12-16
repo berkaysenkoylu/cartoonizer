ac1437822b107812cee3cf8d2169c47294d344da
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2019-12-16 14:46:10 +0300

    Updates

---
+++ precache-manifest.927340d4a2b70f7a667838571b0d91fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2035883cd73fafa11227d88926baf851",
+    "url": "/cartoonizer/index.html"
+  },
+  {
+    "revision": "4fd763779e158696f6ef",
+    "url": "/cartoonizer/static/css/main.0d1bf9a0.chunk.css"
+  },
+  {
+    "revision": "9342259514a64ac340a1",
+    "url": "/cartoonizer/static/js/2.06067ebc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/cartoonizer/static/js/2.06067ebc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fd763779e158696f6ef",
+    "url": "/cartoonizer/static/js/main.56273a5f.chunk.js"
+  },
+  {
+    "revision": "bc3fdb299ca6d48532a3",
+    "url": "/cartoonizer/static/js/runtime-main.c0f04e8e.js"
+  }
+]);
